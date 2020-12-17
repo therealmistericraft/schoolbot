6a9ac66260ca3f494a8d478894e5f41b0fd6065f
therealmistericraft <65766422+therealmistericraft@users.noreply.github.com>
2020-12-17 18:15:00 +0100

    Started to write code
    Decided to use directly custom prefixes (and languages).
    
    Code does not work right now!

--- schoolbot.py
+++ schoolbot.py
@@ -1,1 +1,32 @@
-#TODO: write code
+# coding=utf-8
+
+#1 imports
+import discord
+from discord.ext import commands
+from discord.ext import tasks
+from discord.utils import get
+from time import sleep
+import threading
+import json
+
+#2 settings
+
+#2.1 Activate Intents from API
+intents = discord.Intent.defaults()
+intents.members = True
+intents.reactions = True
+
+#3 instance variables
+prefix = ""
+lang = ""
+
+#4 initalizing
+
+#4.1 prefix
+prefixfile = open('prefix.txt', 'r')
+data = prefixfile.read()
+data = data.split('\n')
+custom_prefixes = data[0]
+custom_prefixes = ast.literal_eval(custom_prefixes)
+default_prefix = '%'
+prefixfile.close()
