61137fb4af8e71a3e14dcf723970fc5137a0ba27
therealmistericraft <65766422+therealmistericraft@users.noreply.github.com>
2021-02-15 17:03:02 +0100

    Tried to fix #12 but failed

--- resources/src/courses/read_courselist.py
+++ resources/src/courses/read_courselist.py
@@ -3,9 +3,17 @@
 import discord
 from discord.ext import commands
 import json
+import sys
+sys.path.append("../")
+import schoolbot as main
 
 def read(message, list):
-    file = open(f".../data/usr/courselists/{message.guild.id}.json", "w+")
+    try:
+        file = open(f".../data/usr/courselists/{message.guild.id}.json", "x")
+    else:
+        #TODO: How to send File without await/async, file does not get overwritten if exists
+        message.channel.send(content=None, embed=discord.Embed(title=main.lang["3"], colour=discord.Colour.red()))
+        message.channel.send(discord.File(f".../data/usr/courselists/{message.guild.id}.json"))
     # IMPORTANT: no such file or directory (maybe, the folder has to exist)
     content = json.load(list)
     json.dump(content, file, indent=4)
